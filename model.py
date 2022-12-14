cc5e1bce49cb0046719eb0dfbe16bceb35a9dbef
Michael Tao <michaeltao0618@gmail.com>
2022-12-13 18:48:40 -0800

    minor update

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 x = np.array(processData())
 y = np.array([0] * 120 + [1] * 120)
 
-X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=.2)
+# X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=.2)
 
 
 model = Sequential()
